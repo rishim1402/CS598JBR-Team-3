4cb834283eeb336bf921c0523cbaccf3823c2edb
LALAYANG <56383562+LALAYANG@users.noreply.github.com>
2024-09-08 19:16:35 -0400

    Update MP2/commands.py

--- MP2/commands.py
+++ MP2/commands.py
@@ -3,31 +3,30 @@
 ###################################################################
 
 # TODO: Update Your NetIDs in alphabetical order
-NetIDs = ["sampleID1", "sampleID2", "sampleID3", "sampleID4"]
-NetIDs_str = " ".join(NetIDs)
+# NetIDs = ["sampleID1", "sampleID2", "sampleID3", "sampleID4"]
+# NetIDs_str = " ".join(NetIDs)
 
 # TODO: Clone your GitHub repository
 ! git clone [Your GitHub Link]
 %cd [Your GitHub Repo]
 
 # Set up requirements for dataset generation
-! bash -x MP2/setup_dataset.sh
+# ! bash -x MP2/setup_dataset.sh
 
-# dataset generation
-! python3 MP2/dataset_generation.py {NetIDs_str} |& tee dataset_generation.log
-
-seed = "<your_seed>"
-# TODO: Replace the file path of selected_humaneval_[seed].jsonl generated in previous step
-input_dataset = "selected_humaneval_" + seed + ".jsonl"
+# Use the same dataset generated in MP1
+input_dataset = "selected_humaneval_**.jsonl"
 
 # Set up requirements for model prompting
 ! bash -x MP2/setup_models.sh
 
+task_1_json = "task_1_" + seed + ".jsonl"
+task_2_json = "task_2_" + seed + ".jsonl"
 task_3_json = "task_3_" + seed + ".jsonl"
 
 # Prompt the models
+! python3 MP2/task_1.py {input_dataset} "deepseek-ai/deepseek-coder-6.7b-instruct" {task_1_json} "True" |& tee task_1_prompt.log
+! python3 MP2/task_2.py {input_dataset} "deepseek-ai/deepseek-coder-6.7b-instruct" {task_2_json} "True" |& tee task_2_prompt.log
 ! python3 MP2/task_3.py {input_dataset} "deepseek-ai/deepseek-coder-6.7b-instruct" {task_3_json} "True" |& tee task_3_prompt.log
-
 ! evaluate_functional_correctness {task_3_json} |& tee task_3_evaluate.log
 
 %cd ..
